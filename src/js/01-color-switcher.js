347760dfca15a04d5e97b748af2e45540eb79761
UlyanaYashan <ulyanalyala@gmail.com>
2023-02-26 19:25:28 +0200

    процес виконання 1 задачі

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const elBtnStart = document.querySelector('button[data-start]');
+const elBtnStop = document.querySelector('button[data-stop]');
+const elBody = document.querySelector('body');
+
+elBtnStop.disabled = true;
+
+elBtnStart.addEventListener('click', onStart);
+elBtnStop.addEventListener('click', onStop);
+
+function onStart() {
+timerID = setInterval(() => {
+elBody.style.backgroundColor = getRandomHexColor();
+},1000)
+
+elBtnStart.disabled = true;
+elBtnStop.disabled = false;
+}
+
+function onStop() {   
+    clearInterval(timerID);
+    elBtnStart.disabled = false;
+    elBtnStop.disabled = true;
+}
